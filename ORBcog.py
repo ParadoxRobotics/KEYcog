359dbb478a8b7dd3275b6d8da66e8d66b8bf3a19
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-06-23 16:09:35 +0200

    Update ORBcog.py
    code skeleton for model creation

--- ORBcog.py
+++ ORBcog.py
@@ -8,14 +8,29 @@ class targetModel():
         # input
         self.targetImage = None # image to train on
         self.targetMask = None # target binary mask
+        self.targetSize = None
         # image keypoint extractor
         self.orb = cv2.ORB_create(nfeatures=nbPointMax)
         self.kp = None
         self.des = None
+        # parameters for the probabilistic model
 
     def createModel(self, img, mask):
-        # store image and mask
+        # store image, mask, size
         self.targetImage = img
         self.targetMask = mask
+        self.targetSize = img.shape
         # compute ORB keypoint and descriptor
         kp, des = self.orb.detectAndCompute(self.targetImage, None)
+        # convert kp and get orientation and position in the image
+
+        # compute the midle of the cropped object or the image
+
+        # for each ORB keypoint
+            # store the angle of the line between the ORB point and the middle point
+
+            # store the length of the line between the ORB point and the middle point devide by the scale of the ORB descriptor
+
+            # store the major orientation of the ORB point descriptor
+
+            # store scale of the ORB point descriptor
