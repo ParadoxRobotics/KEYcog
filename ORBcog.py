9189f8f26b14e183905d50ea21150fd2ef3f6882
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-06-25 16:02:52 +0200

    Update ORBcog.py
    add complete model creation + matching method using KNN and Lowe ratio
    TODO : implement best bin first algorithm

--- ORBcog.py
+++ ORBcog.py
@@ -5,7 +5,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib import pyplot
-from scipy.spatial import Delaunay, delaunay_plot_2d
 
 # create base model
 class targetModel():
@@ -14,6 +13,7 @@ class targetModel():
         # input
         self.targetImage = None # image to train on
         self.targetMask = None # target binary mask
+        self.maxSize = None
         # image keypoint extractor
         self.orb = cv2.ORB_create(nfeatures=nbPointMax)
         # parameters for the probabilistic model
@@ -29,6 +29,7 @@ class targetModel():
         # store target image and mask
         self.targetImage = img
         self.targetMask = mask
+        self.maxSize = np.max(self.targetImage.shape)
         # compute ORB keypoint and descriptor
         kp, des = self.orb.detectAndCompute(self.targetImage, None)
         # store the descriptor
@@ -45,14 +46,14 @@ class targetModel():
             for i in range(0,len(kp)):
                 if np.round(kp[i].pt) in maskpoint:
                     self.pt.append(kp[i].pt)
-                    self.angle.append(kp[i].angle)
+                    self.angle.append(-kp[i].angle*np.pi/180)
                     self.scale.append(kp[i].size)
                     self.response.append(kp[i].response)
         else:
             # get point, angle and size from keypoints
             for i in range(0,len(kp)):
                 self.pt.append(kp[i].pt)
-                self.angle.append(kp[i].angle)
+                self.angle.append(-kp[i].angle*np.pi/180)
                 self.scale.append(kp[i].size)
 
         if self.targetMask is not None and imgCenter == False:
@@ -66,9 +67,42 @@ class targetModel():
         # for every ORB point
         for i in range(0, len(self.pt)):
             # store the angle of the line between the ORB point and the middle point
-            self.angleCenter.append(np.arctan2(self.pt[i][1], self.pt[i][0])-(-self.angle[i]*np.pi/180))
+            self.angleCenter.append(np.arctan2(self.pt[i][1], self.pt[i][0])-self.angle[i])
             # store the length of the line between the ORB point and the middle point devide by the scale of the ORB descriptor
             self.scaleCenter.append(np.sqrt((centerW-self.pt[i][0])**2+(centerH-self.pt[i][1])**2)/self.scale[i])
+
+# match model with current image using KNN matching and Best bin first method 
+def matchModel(targetModel, currentImg, nbPointMax, LoweCoeff):
+    # create ORB descriptor/detector and BF feature matcher
+    orb = cv2.ORB_create(nfeatures=nbPointMax)
+    matcher = cv2.DescriptorMatcher_create("BruteForce-L1")
+    # compute ORB keypoint and descriptor
+    kp, des = orb.detectAndCompute(currentImg, None)
+    # draw keypoint
+    kp_img = cv2.drawKeypoints(currentImg, kp, None,  flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    plt.imshow(kp_img)
+    plt.show()
+    # store current point, angle and scale
+    curPt = []
+    curAngle = []
+    curScale  = []
+    curResponse = []
+    for i in range(0,len(kp)):
+        self.pt.append(kp[i].pt)
+        self.angle.append(-kp[i].angle*np.pi/180)
+        self.scale.append(kp[i].size)
+        self.response.append(kp[i].response)
+    # using BF compute match
+    matches = matcher.knnMatch(queryDescriptors=des, trainDescriptors=targetModel.des, k=2)
+    # filter match using Lowe's method
+    goodMatch = []
+    goodMatchPlot = [] # only for plotting
+    for m,n in matches:
+        if m.distance < LoweCoeff*n.distance:
+            goodMatch.append(m)
+            goodMatchPlot.append([m])
+    # set the location bin size to 0.25 the image model max size
+    maxLoc = targetModel.maxSize*0.25
 
 
 img = cv2.imread('target.jpg')
@@ -76,8 +110,10 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 mask = cv2.imread('mask.png')
 mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 
-model = targetModel(5)
-model.createModel(img, mask, True)
+model = targetModel(nbPointMax=10)
+model.createModel(img=img, mask=mask, imgCenter=True)
+
+
 
 print("point")
 print(model.pt)
@@ -90,6 +126,4 @@ print(model.angleCenter)
 print("scale to center")
 print(model.scaleCenter)
 
-tri = Delaunay(model.pt)
-_ = delaunay_plot_2d(tri)
-plt.show()
+matchModel(targetModel=None, currentImg=img, nbPointMax=10, LoweCoeff=0.70)
