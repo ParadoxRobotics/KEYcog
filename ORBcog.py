f8495405eec1e3b521e91347a2a9a62aba26f512
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-07-09 18:48:30 +0200

    Update ORBcog.py

--- ORBcog.py
+++ ORBcog.py
@@ -1,5 +1,19 @@
 # ORBcog is object template matching method for fast object recognition.
 # This code is based Li Yang Ku SURF object matching
+# Author : Munch Quentin
+
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import numpy as np
 from scipy.linalg import lstsq, norm, inv
