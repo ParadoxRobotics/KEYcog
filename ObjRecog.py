90c06e2bc5b83c94ec9b8cf99df27a36448c569f
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-07-12 13:56:07 +0200

    Add files via upload
    first commit

---
+++ ObjRecog.py
@@ -0,0 +1,39 @@
+# Robust, efficint and simple single objet recognition for robot application
+# use LSD python wrapper : https://github.com/xanxys/lsd-python
+# Author : Munch Quentin, 2020.
+
+import numpy as np
+import cv2
+import lsd
+from matplotlib import pyplot as plt
+from matplotlib import pyplot
+
+# create object model
+class targetModel():
+    def __init__(self, ):
+        # detector class -> only for drawing
+        self.fld = cv2.ximgproc.createFastLineDetector()
+        # base input
+        self.imageTarget = None
+        self.maskTarget = None
+        # model
+        self.LSDLine = None
+        self.descriptor = None
+        self.position = None
+
+    def createModel(self, img, mask):
+        # store input data
+        self.imageTarget = img
+        if mask is not None:
+            self.maskTarget = mask
+        # compute line
+        imgLine_ = lsd.detect_line_segments(self.imageTarget.astype('float64'), scale=0.8, sigma_scale=0.6, quant=2.0, ang_th=22.5, log_eps=0.0, density_th=0.7, n_bins=1024)
+        self.LSDLine = np.reshape(imgLine_[:,0:4], (imgLine_.shape[0],1,4)).astype('float32')
+        # plot
+        plt.imshow(self.fld.drawSegments(self.imageTarget, self.LSDLine), interpolation='nearest', aspect='auto')
+        plt.show()
+
+img = cv2.imread('target.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.resize(img, (640,480))
+model = targetModel()
+model.createModel(img, None)
